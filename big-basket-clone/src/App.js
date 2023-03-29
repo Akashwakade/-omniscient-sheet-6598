78570918e8bad25a132b608289cfbd43dc788edf
akash wakade <mr.akashwakade@gmail.com>
2023-03-29 12:07:29 +0530

    added

---
+++ big-basket-clone/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { Box,Flex,Spacer,Input } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+       
+
+        <Flex margin={'auto'} alignItems={'center'}  bg='blue' w='80%' p={4} color='white'>
+          <Box><h1>image</h1></Box>
+          <Spacer/>
+          <Box width={'600px'} border={'2px solid red' }><Input placeholder='Search for Products...' /></Box>
+          <Spacer/>
+          <Box><h1>image2</h1></Box>
+
+        </Flex>
+       
+    </div>
+  );
+}
+
+export default App;
